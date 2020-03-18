f1c9e6ae54916b2f4131234f54bf19493168a0e5
Shivam Sharma <53265299+shivamksharma@users.noreply.github.com>
2020-03-18 10:05:02 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import speech_recognition as speech_recognition
+import plysound
+from gtts import gtts
+import random
+from time import ctime
+import webbrowser
+import ssl
+import certifi
+importtime
+import os
+
+class person:
+    name = ''
+    def setName(self, name):
+        self.name = name
+        
+def there_exists(terms):
+  for term in terms:
+    if term in voice_data:
+        return True
+        
