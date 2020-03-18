39831caab296243510492f2d2d64eff7820b5c7d
Shivam Sharma <53265299+shivamksharma@users.noreply.github.com>
2020-03-18 11:39:07 +0530

    Update access.py

--- access.py
+++ access.py
@@ -1,1 +1,15 @@
 import speech_recognition
+import playsound
+from gtts import gtts
+import random
+from time import ctime
+import webbrowser
+import ssl
+import certifi
+import time
+import os
+from PIL import Image
+import subrpocess
+import pyautogui
+import pyttsx3
+import bs4 as bs
